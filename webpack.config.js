5733862cce0a1a25a67ac108cdc5bb0e94955de6
Daryna Gavrylenko <deakozyulka@gmail.com>
2017-07-13 19:10:31 +0300

    fix heroku 2

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: __dirname + '/build'
+        path: __dirname + '/public'
     },
     plugins: [ HTMLWebpackPluginConfig, webpackProvidePlugin ]
 };
